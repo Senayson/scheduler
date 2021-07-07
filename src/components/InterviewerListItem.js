c5a87353e9cd0cc430cee51c7a746b2ed5565a67
Senay <senaialema@gmail.com>
2021-07-07 17:24:33 -0400

    update

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import './InterviewerListItem.scss';
-const classNames = require('classnames');
+import React from "react";
+import "./InterviewerListItem.scss";
+const classNames = require("classnames");
 
 export default function InterviewerListItem(props) {
-const { id, name, avatar, selected, setInterviewer} = props;
-const interviewerClass = classNames("interviewers__item-image", 
-{"interviewers__item-image--selected": selected,
-}
-)
-console.log('This is interview class' ,interviewerClass);
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const interviewerClass = classNames("interviewers__item-image", {
+    "interviewers__item--selected": selected,
+  });
+  console.log("This is interview class", interviewerClass);
 
-return (
-<li className="interviewers__item" onClick={()=> setInterviewer(name)}>
-  <img
-    className={interviewerClass}
-    src={avatar}
-    alt={name}
-  />
- {name}
-</li>
-)
+  return (
+    <li className={interviewerClass} onClick={() => setInterviewer(name)}>
+      <img className="interviewers__item-image" src={avatar} alt={name} />
+      {name}
+    </li>
+  );
 }
